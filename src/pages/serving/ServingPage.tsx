07a16c469fce12e949cfbcce902641c9eb348077
gn00py48 <gn00py48@gmail.com>
2026-02-26 11:12:20 +0900

    servingPage ui

---
+++ src/pages/serving/ServingPage.tsx
@@ -0,0 +1,13 @@
+import * as S from "./ServingPage.styled";
+
+
+const ServingPage = () => {
+
+    return (
+        <>
+            <S.Wrapper>
+                
+            </S.Wrapper>
+        </>
+    );
+}
